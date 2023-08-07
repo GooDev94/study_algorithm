36d7553a79a6e042460df6a77d2f6fbb359d15b7
choco <choco@goodong>
2023-08-07 17:22:11 +0900

    스택과 큐 실전문제

---
+++ study_algorithm/src/study_algorithm/data_structure/Baekjun_2164.java
@@ -0,0 +1,39 @@
+package study_algorithm.data_structure;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Baekjun_2164 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		//절댓값 힙
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> MyQueue = new PriorityQueue<>((o1, o2) -> {
+			int first_abs = Math.abs(o1);
+			int second_abs = Math.abs(o2);
+			
+			if(first_abs == second_abs) {
+				return o1 > o2 ? 1 : -1;
+			}else {
+				return first_abs - second_abs;
+			}
+					
+		});
+		
+		for(int i = 0; i<N; i++) {
+			int request = Integer.parseInt(br.readLine());
+			if(request == 0) {
+				if(MyQueue.isEmpty()) {
+					System.out.println("0");
+				}else {
+					System.out.println(MyQueue.poll());
+				}
+			}else {
+				MyQueue.add(request);
+			}
+		}
+	}
+
+}
