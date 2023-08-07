d282b6a192fd8f74a4d13e4b0740d069abdd6cf5
choco <choco@goodong>
2023-08-07 17:29:09 +0900

    오타수정

--- study_algorithm/src/study_algorithm/data_structure/Baekjun_2164.java
+++ study_algorithm/src/study_algorithm/data_structure/Baekjun_2164.java
@@ -1,39 +1,36 @@
 package study_algorithm.data_structure;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Baekjun_2164 {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		//절댓값 힙
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> MyQueue = new PriorityQueue<>((o1, o2) -> {
-			int first_abs = Math.abs(o1);
-			int second_abs = Math.abs(o2);
-			
-			if(first_abs == second_abs) {
-				return o1 > o2 ? 1 : -1;
-			}else {
-				return first_abs - second_abs;
-			}
-					
-		});
+	public static void main(String[] args) {
+		// 스택과 큐 실전문제 - 카드게임
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer> myQueue = new LinkedList<>();
 		
-		for(int i = 0; i<N; i++) {
-			int request = Integer.parseInt(br.readLine());
-			if(request == 0) {
-				if(MyQueue.isEmpty()) {
-					System.out.println("0");
-				}else {
-					System.out.println(MyQueue.poll());
-				}
-			}else {
-				MyQueue.add(request);
-			}
+		int N = sc.nextInt();
+		for(int i = 1; i<=N; i++) {
+			myQueue.add(i);
 		}
+		
+		//한장 밖에 없을 경우 빠져나오기
+		while(myQueue.size()>1) {
+			//맨 위의 카드 한장을 버림
+			myQueue.poll();
+			
+			//맨위의 카드를 가장 아래의 카드 밑으로 이동
+			/* 1번 방법
+			int temp = myQueue.poll();
+			myQueue.add(temp);
+			*/
+			myQueue.add(myQueue.poll());
+			
+		}
+			
+		System.out.println(myQueue.poll());
 	}
+
 
 }
