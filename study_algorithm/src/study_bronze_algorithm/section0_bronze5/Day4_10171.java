709787cc8fddbfdc136bee23583df310d31512dd
gdy <codingnine@gmail.com>
2023-12-11 15:41:13 +0900

    백준온라인저지 bronze5 문제풀이 Day4

--- study_algorithm/src/study_bronze_algorithm/section0_bronze5/Day4_10171.java
+++ study_algorithm/src/study_bronze_algorithm/section0_bronze5/Day4_10171.java
@@ -4,11 +4,10 @@ public class Day4_10171 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("|\\_/|");
-		System.out.println("|q p|   /}");
-		System.out.println("( 0 )\"\"\"\\");
-		System.out.println("|\"^\"`    |");
-		System.out.println("||_/=\\\\__|");
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
 	}
 
 }
