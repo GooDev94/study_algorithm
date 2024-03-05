9cff2e81032308db3d2dcc6a92415b9c72b8501e
gdy <codingnine@gmail.com>
2024-03-05 19:25:37 +0900

    [24.03.03] 주석 추가

--- study_algorithm/src/study_programmers_algorithm/Programmers_120818.java
+++ study_algorithm/src/study_programmers_algorithm/Programmers_120818.java
@@ -2,6 +2,10 @@ package study_programmers_algorithm;
 
 public class Programmers_120818 {
 
+	/*
+	 *  코딩테스트 연습 > 코딩테스트 입문 > 옷가게 할인 받기
+	 * 
+	 * */
 	public static void main(String[] args) {
 		System.out.println(solution(150000));
 	}
--- study_algorithm/src/study_programmers_algorithm/Programmers_120854.java
+++ study_algorithm/src/study_programmers_algorithm/Programmers_120854.java
@@ -2,6 +2,9 @@ package study_programmers_algorithm;
 
 public class Programmers_120854 {
 
+	/*
+	 * 코딩테스트 연습 > 코딩테스트 입문 > 배열 원소의 길이
+	 * */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] txt = {"We", "are", "the", "world!"};
